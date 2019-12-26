02a69bd11dbba4ceb3a4231c2ce9907efe90f5e1
hezhensheng3 <1569929533@qq.com>
2019-12-26 16:25:47 +0800

    feat: NIO实现通过channel读取文件到buffer中

--- study/study-basic/src/main/java/com/hzs/nio/ChannelDemo.java
+++ study/study-basic/src/main/java/com/hzs/nio/ChannelDemo.java
@@ -1,12 +1,14 @@
 package com.hzs.nio;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.HashMap;
 
 /**
- * TODO
+ * Channel测试类
  *
  * @author hezhensheng3
  * @date 2019-12-25 21:16
@@ -14,14 +16,47 @@ import java.nio.channels.FileChannel;
 public class ChannelDemo {
 
     public static void main(String[] args) {
-
+        readDataByChannel();
     }
 
-    public static void readDataByChannel() throws FileNotFoundException {
-        RandomAccessFile accessFile = new RandomAccessFile("E:/hezhensheng/study/nio/nio-data.txt", "rw");
-        FileChannel fileChannel = accessFile.getChannel();
-        ByteBuffer buffer = ByteBuffer.allocate(48);
-
+    /**
+     * 读取数据通过channel
+     */
+    public static void readDataByChannel() {
+        RandomAccessFile accessFile = null;
+        try {
+            accessFile = new RandomAccessFile("E:/hezhensheng/study/nio/nio-data.txt", "rw");
+            FileChannel fileChannel = accessFile.getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate(48);
+            // 1.获取字节数
+            int bytesRead = fileChannel.read(buffer);
+            while (bytesRead != -1) {
+                System.out.println("Read = " + bytesRead);
+                /**
+                 * 2.锁定读的范围 0-8
+                 * limit = position
+                 * position = 0
+                 */
+                buffer.flip();
+                /**
+                 * 3.如果position < limit
+                 */
+                while (buffer.hasRemaining()) {
+                    // 4.position加1
+                    System.out.println((char)buffer.get());
+                }
+                buffer.clear();
+                bytesRead = fileChannel.read(buffer);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                accessFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
